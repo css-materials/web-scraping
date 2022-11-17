580b220eeb4abac7978595a0453da252f8a2ca7c
Sabrina Nardin <nardin@uchicago.com>
2022-11-17 15:17:23 -0600

    First commit

---
+++ 01-best-places.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(rvest)
+
+## SPERLING'S BEST PLACES 
+
+
+# TASK 1: Look up the cost of living for your hometown on 
+# Sperling's Best Places website: http://www.bestplaces.net/ 
+# Then, extract it with `html_elements()` and `html_text2()`
+
+
+
+
+# TASK 2: Get the first table and convert it to a data frame
+# To exact tables use the rvest function html_table()
+
+
+
+# TASK 3: Extract the climate statistics table of your hometown
+
---
+++ 02-scrape-imdb.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(rvest)
+
+# EXTRACT MOVIE INFORMATION FROM IMDb - EXAMPLE WITH FROZEN
+
+
+# Read in the Frozen html page (https://www.imdb.com/title/tt2294629/)
+# Store it in an object called `frozen`
+
+
+# Select the nodes that are both `a`s and `id = "titleCast"`
+
+
+# Extract the text from the nodes
+
+
+# Use SelectorGadget to find a CSS selector combination that
+# identifies just the cast member names
+
+
+# Clean up text
